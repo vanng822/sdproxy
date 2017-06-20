6c262b275c231c3cb817ecd3035ea80082b52b40
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2017-06-20 11:54:43 +0700

    #3 creating server from config

---
+++ config.go
@@ -0,0 +1,40 @@
+package sdproxy
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type LocationConfig struct {
+	Path    string
+	Servers []string
+}
+
+type Config struct {
+	Addr      string
+	Locations []*LocationConfig
+}
+
+func NewServerFromConfig(conf *Config) *Server {
+	var locations []*Location
+	for _, location := range conf.Locations {
+		locations = append(locations, NewLocation(location.Path, NewUpstream(location.Servers...)))
+	}
+	server := NewServer(locations...)
+	return server
+}
+
+func ParseConfig(filename string) *Config {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	conf := Config{}
+	err = decoder.Decode(&conf)
+	if err != nil {
+		panic(err)
+	}
+	return &conf
+}
---
+++ config_test.go
@@ -0,0 +1,41 @@
+package sdproxy
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewServerFromConfig(t *testing.T) {
+	conf := Config{
+		Addr: "127.0.0.1:8080",
+		Locations: []*LocationConfig{
+			&LocationConfig{
+				Path: "/",
+				Servers: []string{
+					"127.0.0.1:8081",
+					"127.0.0.1:8082"},
+			},
+			&LocationConfig{
+				Path: "/api",
+				Servers: []string{
+					"127.0.0.1:8083",
+					"127.0.0.1:8084"},
+			},
+		},
+	}
+
+	server := NewServerFromConfig(&conf)
+
+	assert.Equal(t, "/api", server.locations[0].path)
+	assert.Equal(t, "/", server.locations[1].path)
+}
+
+func TestParseConfig(t *testing.T) {
+	conf := ParseConfig("config.json")
+	assert.Equal(t, 1, len(conf.Locations))
+	assert.Equal(t, "127.0.0.1:8080", conf.Addr)
+	location := conf.Locations[0]
+	assert.Equal(t, "/api", location.Path)
+	assert.Equal(t, []string{"127.0.0.1:8090", "127.0.0.1:8091"}, location.Servers)
+}
