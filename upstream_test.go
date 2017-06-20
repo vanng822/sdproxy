7bd22670b0581bcd57a034b9332fe7d4076aa3e8
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2017-06-20 09:42:35 +0700

    Test next upstream

---
+++ upstream_test.go
@@ -0,0 +1,28 @@
+package sdproxy
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNextEndPoint(t *testing.T) {
+	r1 := &httputil.ReverseProxy{Director: func(req *http.Request) {
+		req.URL.Scheme = "http"
+		req.URL.Host = "127.0.0.1:8091"
+	}}
+	r2 := &httputil.ReverseProxy{Director: func(req *http.Request) {
+		req.URL.Scheme = "http"
+		req.URL.Host = "127.0.0.1:8092"
+	}}
+
+	up := NewUpstream(r1, r2)
+	res := up.nextEndPoint()
+	assert.Equal(t, r1, res)
+	res = up.nextEndPoint()
+	assert.Equal(t, r2, res)
+	res = up.nextEndPoint()
+	assert.Equal(t, r1, res)
+}
